6f59aa0ee38d009f444dd758017d78cef6e72f28
Weverson Muciarone <muciaronew@arriva.co.uk>
2024-11-06 15:07:15 +0000

    Corrected indentation of layout

--- app1/Bonus/compress_file.py
+++ app1/Bonus/compress_file.py
@@ -12,8 +12,8 @@ button2 = sg.FolderBrowse("Choose")
 compress_button = sg.Button("Compress")
 
 window = sg.Window("File Compressor", layout=[[label1, input1, button1],
-                                              [label2, input2, button2],
-                                              [compress_button]])
+                                                  [label2, input2, button2],
+                                                  [compress_button]])
 
 window.read()
 window.close()
